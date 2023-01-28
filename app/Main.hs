0927cbe73c8f62751bb2dc9c06c4ad99f65235c8
ivy bridges <ivy.v.bridges@gmail.com>
2023-01-28 12:38:56 -0500

    cleanup and removing obsolete copying functions

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,11 @@ import qualified Codec.Archive.Tar as Tar
 import qualified Data.ByteString.Lazy as BS
 import qualified Codec.Compression.GZip as GZ
 
-
+main :: IO ()
+main = do
+    modlist:args <- getArgs -- get name of cfg file
+    
+    packCFG modlist
 
 
 -- packs files from a cfg into a compressed tar folder
@@ -23,8 +27,7 @@ packCFG modlist = do
     
     let kartFolder = head paths
         dlFolder = subDirectory kartFolder "Download"
-        
-        -- chop off the .cfg extension and replace with .tar
+        -- chop off the .cfg extension and replace with .tar.gz
         tarDestination = take (length modlist - 4) modlist ++ ".tar.gz"
     
    
@@ -34,16 +37,12 @@ packCFG modlist = do
     
     -- some are stored in the home folder (cfgs, socs, bonuschars) while some are in downloads
     let (homeFiles, dlFiles) = partition storedInHome files 
-    
-    
-    
     -- grabs the files from home
     -- and files from download
     homeEntries <- Tar.pack kartFolder homeFiles -- Tar.pack :: FilePath -> [FilePath] -> IO [Entry]
     dlEntries   <- Tar.pack dlFolder   dlFiles 
     
-    
-    let fullArchive = homeEntries ++ dlEntries
+    let fullArchive = homeEntries ++ dlEntries -- combine and compress the archive
         compressedTar = (GZ.compress . Tar.write) fullArchive
     
     putStrLn "Packing .tar archive..."
@@ -51,11 +50,7 @@ packCFG modlist = do
     
     
     
-main :: IO ()
-main = do
-    modlist:args <- getArgs -- get name of cfg file
-    
-    packCFG modlist
+
     
 
 
@@ -63,50 +58,30 @@ main = do
 readsettings :: IO [String]
 readsettings = do
     contents <- readFile "settings.txt"
-    
-    
-    let paths = lines contents
-    
-    return paths
+    return (lines contents)
 
 -- grab cfgs and addons from a cfg file
 modfiles :: FilePath -> IO [String]
 modfiles filename = do
     putStrLn ("Grabbing filenames from " ++ filename ++ "...")
     contents <- readFile filename
-    let commands = lines contents  
-        execs = filter (\i -> take 4 i == "exec") commands -- only lines starting with exec. these denote additional cfg files
-        cfgs = map (drop 5) execs -- just get the filename
+    let commands = lines contents
         
-        addfiles = filter (\i -> take 7 i == "addfile") commands -- only lines starting with "addfile"
-        files = cfgs ++ (map (drop 8) addfiles) -- just get the filename
+        -- we need the cfg and soc files as well as any mods it adds
+        cfgs     = grabCommands "exec" commands        
+        socs     = grabCommands "runsoc" commands
+        addfiles = grabCommands "addfile" commands
         
-        runsocs = filter (\i -> take 6 i == "runsoc") commands -- only lines starting with runsoc. these denote soc files
-        socs = map (drop 7) runsocs -- just get the file name
+        files = cfgs ++ socs ++ addfiles
     
     -- recurses into referenced cfg files and adds them to mods list
     additionals <- mapM modfiles cfgs
-    
-    return (nub $ concat [files,socs, concat additionals]) -- combine the additionals with the base list and wrap them for IO
-    
+    -- combine the additionals with the base list and wrap them for IO, removing duplicates
+    return (nub $ concat [files,socs, concat additionals]) 
     
 
 
-copymod :: String -> String -> String -> IO ()
-copymod home dir mod = do
-    let copyDest = if isCFG mod 
-                   then subDirectory dir ("CFGs/"++mod)
-                   else subDirectory dir mod
-    
-    alreadyThere <- doesFileExist copyDest
-    --putStrLn mod
-    --print (storedInHome mod)
-    
-  
-    unless alreadyThere (do 
-        if storedInHome mod
-            then withCurrentDirectory home (copyFile mod copyDest)
-        else copyFile mod copyDest)
+-- convenience functions
 
 -- cfg, soc, and the bonuschars.kart files are all stored in home instead of /Download/
 storedInHome :: FilePath -> Bool
@@ -117,11 +92,11 @@ storedInHome file
     
 
 
+-- given a prefix to match and a list of commands, returns all matching lines with the prefix removed
+grabCommands :: String -> [String] -> [String]
+grabCommands prefix commands = map (drop $ length prefix + 1) matches
+    where matches = filter (isPrefixOf prefix) commands
 
-
-
-isCFG :: FilePath -> Bool
-isCFG file = drop (length file - 3) file == "cfg"
 
 -- i wrote this because i am sick of writing ++"/" to piece together folders manually
 subDirectory :: FilePath -> String -> FilePath
